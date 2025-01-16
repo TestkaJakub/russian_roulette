fb1fb3c1648483e9d66d05a0b8dcd9363cc6b381
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-01-16 23:39:58 +0100

    weak reference

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,82 @@
 // Type your code here, or load an example.
-use std::{cell::RefCell, rc::Rc, ops::Shr};
+use std::{cell::RefCell, ops::Shr, rc::{Rc, Weak}};
 
+#[derive(Debug)]
 struct Player {
     name: String,
-    next_player: Option<Rc<RefCell<Player>>>,
+    next_player: Option<Weak<RefCell<Player>>>,
 }
 
 impl Player {
-    fn shoot(&self, cylinder : u8) -> bool {
+    fn new(name: &str) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Player {
+            name: name.to_string(),
+            next_player: None,
+        }))
+    }
+
+    fn shoot(&self, cylinder : u128) -> bool {
         if cylinder % 2 == 1 {
             return true;
         }
         false
     }
 
-    fn play(&self, cylinder : u8) {
-        if cylinder <= 0 {
-            println!("No bullets left, game ends!");
-            return;
-        }
+    fn play_one_turn(&self, cylinder : u128) -> Option<u128> {
 
         if self.shoot(cylinder) {
             println!("Player {} died!", self.name);
         }
-
-        match &self.next_player {
-            Some(v) => v.borrow().play(cylinder.shr(1)),
-            None => println!("Game ends!")
-        }
         
+        self.next_player.as_ref()
+                        .and_then(|x| x.upgrade())
+                        .and_then(|x| x.try_borrow_mut()
+                                                            .ok()
+                                                            .and_then(|x| x.play_one_turn(cylinder.shr(1))))
+                        .or_else(|| Some(cylinder.shr(1))) 
+                        
     }
+
+    fn play_roulette(&self, cylinder: Option<u128>) -> Option<u128> {
+        match cylinder.and_then(|v: u128| 
+            {
+                if v > 0
+                {
+                    self.play_one_turn(v)
+                }
+                else 
+                {
+                    None
+                }
+            } 
+        )
+                .or_else(|| None) 
+                {
+                    Some(v) if v > 0 => self.play_roulette(Some(v)),
+                    _ => { 
+                        println!("No bullets left, game ends!");
+                        None
+                    }
+                }
+    }
+
+
 }
 
 fn main() {
-    let player1 = Rc::new(RefCell::new(Player {
-        name: "p1".to_string(),
-        next_player: None,
-    }));
+    let player1 = Player::new("p1");
+    let player2 = Player::new("p2");
+    let player3 = Player::new("p3");
+    let player4 = Player::new("p4");
+    let player5 = Player::new("p5");
+    let player6 = Player::new("p6");
 
-    let player2 = Rc::new(RefCell::new(Player {
-        name: "p2".to_string(),
-        next_player: Some(player1.clone()),
-    }));
+    player1.borrow_mut().next_player = Some(Rc::downgrade(&player2));
+    player2.borrow_mut().next_player = Some(Rc::downgrade(&player3));
+    player3.borrow_mut().next_player = Some(Rc::downgrade(&player4));
+    player4.borrow_mut().next_player = Some(Rc::downgrade(&player5));
+    player5.borrow_mut().next_player = Some(Rc::downgrade(&player6));
+    player6.borrow_mut().next_player = Some(Rc::downgrade(&player1));
 
-    let player3 = Rc::new(RefCell::new(Player {
-        name: "p3".to_string(),
-        next_player: Some(player2.clone()),
-    }));
-
-    let player4 = Rc::new(RefCell::new(Player {
-        name: "p4".to_string(),
-        next_player: Some(player3.clone()),
-    }));
-
-    let player5 = Rc::new(RefCell::new(Player {
-        name: "p5".to_string(),
-        next_player: Some(player4.clone()),
-    }));
-
-    player1.borrow_mut().next_player = Some(player5.clone());
-
-    player1.borrow().play(0b10000);
+    player1.borrow_mut().play_roulette(Some(0b100000000000));
 }
